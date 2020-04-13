3411905d2028ac7078c355fe1fdac9edcb6140d5
SharonRose C <sharonrose@SharonRoses-MacBook-Pro.local>
2020-04-13 16:36:10 -0400

    hint expressions

--- TripleX.cpp
+++ TripleX.cpp
@@ -4,14 +4,12 @@ using namespace std;
 
 int main ()
 {
-   
+    //Print welcome messages to the terminal.
     cout << "Congrats Agent, you have been assigned a case! You need to break into a secure server room." <<endl;
-     // printf (" You need to enter the correct codes to continue!");
-     //std:: cout << std:: endl;
-     cout << "You need to enter the correct codes to continue!" << endl;
+    cout << "You need to enter the correct codes to continue!" << endl;
 
  
-
+    //Declare 3 number code
     const int a = 4;
     const int b = 3;
     const  int c = 2; 
@@ -19,9 +17,16 @@ int main ()
     const int sum = a + b + c;
     const int product = a * b * c;
 
-    
-    cout << sum << endl;
-    cout << product << endl;
+    //print sum and product to terminal 
+   cout << "There are 3 numbers in the code." << endl;
+    cout << "The codes add up to " sum << endl;
+    cout << "The codes multiple out to " product << endl;
 
     return 0; 
 }
+
+ /* 
+     printf (" You need to enter the correct codes to continue!");
+     std:: cout << std:: endl;
+
+*/
