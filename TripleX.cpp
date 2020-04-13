3c86f37931c94925994b7b97c628cc2def65d13f
SharonRose C <sharonrose@SharonRoses-MacBook-Pro.local>
2020-04-13 16:11:22 -0400

    adding const variable

--- TripleX.cpp
+++ TripleX.cpp
@@ -7,8 +7,21 @@ int main ()
    
     cout << "Congrats Agent, you have been assigned a case! You need to break into a secure server room." <<endl;
      // printf (" You need to enter the correct codes to continue!");
-    //std:: cout << std:: endl;
+     //std:: cout << std:: endl;
      cout << "You need to enter the correct codes to continue!" << endl;
+
+ 
+
+    const int a = 4;
+    const int b = 3;
+    const  int c = 2; 
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    
+    cout << sum << endl;
+    cout << product << endl;
 
     return 0; 
 }
