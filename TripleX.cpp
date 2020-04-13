644767c31e73bd15364c58cc879fa620149ee013
SharonRose C <sharonrose@SharonRoses-MacBook-Pro.local>
2020-04-13 00:53:53 -0400

    print

--- TripleX.cpp
+++ TripleX.cpp
@@ -4,5 +4,10 @@
 
 int main ()
 {
+    std:: cout << "Congrats Agent, you have been assigned a case and you need to break into a secure server room.";
+    //checking if other print style from c works
+    printf (" You need to enter the correct codes to continue!");
+
+
     return 0; 
 }
