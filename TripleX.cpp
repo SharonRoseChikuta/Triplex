b28e2bd05a20b9202e085e9b0e3dbd491ccd3390
SharonRose C <sharonrose@sharonroses-mbp.myfiosgateway.com>
2021-11-03 23:03:43 -0400

    switching escape sequences

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main ()
 {
     //Print welcome messages to the terminal.
-    cout << endl<< "Congrats Assasin, you have been assigned a case!" <<endl;
-    cout << "You need to break into the Cyclop's lair and kill him in his sleep." <<endl;
-    cout << "Enter the correct codes to his lair before the alram sets off, alerting his bandits." << endl << endl << endl;
+    cout << endl<< "Congrats Assasin, you have been assigned a case! \n";
+    cout << "You need to break into the Cyclop's lair and kill him in his sleep \n";
+    cout << "Enter the correct codes to his lair before the alram sets off, alerting his bandits.\n\n\n" ;
 
 
     //Declare 3 number code
@@ -19,7 +19,7 @@ int main ()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     //print sum and product to terminal 
-    cout << "*** There are 3 numbers in the code." << endl;
+    cout << "*** There are 3 numbers in the code.\n"; 
     cout << "*** The codes add up to: " << CodeSum << endl;
     cout << "*** The codes multiple out to give: " << CodeProduct << endl << endl;
 
@@ -37,12 +37,12 @@ int main ()
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
     //if this statement returns false it will skip the code and move on to the next line 
-    cout << "You're in, goodluck!" << endl << endl;
+    cout << "You're in, goodluck! \n" ;
     }
     else 
     {
         //how can I change the code so that the guesses are limited, like "You have three tries left!"
-        cout << "You have failed. The bandits are coming, run!" <<endl << endl;
+        cout << "You have failed. The bandits are coming, run \n";
     }
 
 
